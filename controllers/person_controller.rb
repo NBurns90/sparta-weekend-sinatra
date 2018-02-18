3a622b69d3ee2cb9bd97cc0d7c742b13bec7adbb
NBurns90 <natburns@gmail.com>
2018-02-18 15:41:07 +0000

    index, show and database working

--- controllers/person_controller.rb
+++ controllers/person_controller.rb
@@ -10,4 +10,69 @@ class PersonController < Sinatra::Base
       register Sinatra::Reloader
   end
 
+  # index
+  get '/person' do
+
+    @title = "People"
+    @persons = Person.all
+
+    erb :'person/index'
+  end
+
+  # new
+  get 'person/new' do
+
+    @person = Person.new
+
+    erb :'person/new'
+  end
+
+  # show
+  get '/person/:id' do
+    id = params[:id].to_i
+
+    @person = Person.find(id)
+
+    erb :'person/show'
+  end
+
+  # create
+  post '/person' do
+
+    person = Person.new
+
+    # bind the values
+    person.avatar = params[:avatar]
+    person.first_name = params[:first_name]
+    person.last_name = params[:last_name]
+    person.gender = params[:gender]
+    person.city_id = params[:city_id]
+
+    # save the post
+    person.save
+
+    redirect '/person'
+  end
+
+  # update
+  put '/:id' do
+    id = params[:id].to_i
+
+    redirect '/'
+  end
+
+  # delete
+  delete '/:id' do
+    id = params[:id].to_i
+
+    redirect '/'
+  end
+
+  # edit
+  get '/:id/edit' do
+    id = params[:id].to_i
+
+    erb :'person/edit'
+  end
+
 end
--- models/person.rb
+++ models/person.rb
@@ -1,60 +1,71 @@
 class Person
-  attr_accessor :id, :avatar, :first_name, :last_name, :gender, :country_id
+  attr_accessor :id, :avatar, :first_name, :last_name, :gender, :city_id
 
   def self.open_connection
     conn = PG.connect(dbname: "sinatraweekend")
   end
 
-end
+  def self.all
+    conn = self.open_connection
 
-#   def self.all
-#     conn = self.open_connection
-#
-#     sql = "SELECT id, title, body FROM post ORDER BY id"
-#
-#     results = conn.exec(sql)
-#
-#     posts = results.map do |tuple|
-#       self.hydrate tuple
-#     end
-#   end
-#
-#   def self.find id
-#     conn = self.open_connection
-#
-#     sql = "SELECT id, title, body FROM post WHERE id=#{id}"
-#
-#     results = conn.exec(sql)
-#
-#     post = self.hydrate results[0]
-#   end
-#
-#   def save
-#     conn = Post.open_connection
-#
-#     if(!self.id)
-#       sql = "INSERT INTO post (title, body) VALUES ('#{self.title}', '#{self.body}')"
-#     else
-#       sql = "UPDATE post SET title='#{self.title}', body='#{self.body}' WHERE id=#{self.id}"
-#     end
-#
-#     conn.exec(sql)
-#   end
-#
-#   def self.destroy id
-#       conn = self.open_connection
-#
-#       sql = "DELETE FROM post WHERE id = #{id}"
-#
-#       conn.exec(sql)
-#   end
-#
-#   def self.hydrate post_data
-#     post = Post.new
-#
-#     post.id = post_data['id']
-#     post.title = post_data['title']
-#     post.body = post_data['body']
-#
-#     post
-#   end
+    sql = "SELECT id, avatar, first_name FROM person_data ORDER BY id"
+
+    results = conn.exec(sql)
+
+    persons = results.map do |tuple|
+      self.hydrate tuple
+    end
+
+    persons
+  end
+
+  def self.find id
+    conn = self.open_connection
+
+    sql = "SELECT * FROM person_data WHERE id = #{id} LIMIT 1"
+    # sql = "SELECT id, avatar, first_name, last_name, gender FROM person_data WHERE id=#{id}"
+
+    results = conn.exec(sql)
+
+    person = self.hydrate results[0]
+
+    person
+  end
+
+  def self.destroy id
+      conn = self.open_connection
+
+      sql = "DELETE FROM person_data WHERE id = #{id}"
+
+      conn.exec(sql)
+  end
+
+  # SQL save function
+  def save
+      conn = Person.open_connection
+
+      if(!self.id)
+        # Insert a new record in to the database
+        sql = "INSERT INTO person_data (avatar, first_name, last_name, gender, city_id) VALUES ('#{self.avatar}', '#{self.first_name}', '#{self.last_name}', '#{self.gender}', '#{self.city_id}')"
+      else
+        # Update an existing one
+        sql = "UPDATE person_data SET avatar='#{self.avatar}', first_name='#{self.first_name}', last_name='#{self.last_name}', gender='#{self.gender}', city_id='#{self.city_id}' WHERE id = #{self.id}"
+      end
+
+      conn.exec(sql)
+  end
+
+  # hydrate function
+  def self.hydrate person_data
+    person = Person.new
+
+    person.id = person_data['id']
+    person.avatar = person_data['avatar']
+    person.first_name = person_data['first_name']
+    person.last_name = person_data['last_name']
+    person.gender = person_data['gender']
+
+    person
+  end
+
+end
